6fdc226e179a3aeafdf976a63507d515a6f49006
dennisKim14 <rlaehdtn14@gmail.com>
2022-04-22 11:51:14 +0900

    change subscription

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          It Will Change To Shopping MAll
         </a>
       </header>
     </div>
